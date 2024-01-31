87186b3be97028ea45fda465da96c9284155d2a8
ExclusiveKnight <57048823+ExclusiveKnight@users.noreply.github.com>
2024-01-31 10:24:10 +0100

    Added Main Menu

---
+++ Menu/Main/main-menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Levels do not exist yet
+#@onready var level_scene = preload("res://Menu/Main/main-menu.tscn")
+
+# Start button
+func _on_start_pressed() -> void:
+	#get_tree().change_scene_to_file(level_scene)
+	pass
