bc841ae7a9d994651047ab09b9ebe8f44ae9ce4a
William <william.alm.05@gmail.com>
2024-02-02 12:51:49 +0100

    Added health management

--- Player/player.gd
+++ Player/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health = 100.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
-
+	
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %Hurtbox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%Healthbar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
 
