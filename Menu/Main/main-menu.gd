f2cf23ab6f445d67d53f46b8078686ffcc6517ad
ExclusiveKnight <57048823+ExclusiveKnight@users.noreply.github.com>
2024-02-02 13:09:57 +0100

    Remove AudioManager.gd

--- Menu/Main/main-menu.gd
+++ Menu/Main/main-menu.gd
@@ -3,10 +3,10 @@ extends Control
 # Start button
 # TODO: Load into level scene (possibly a level selector if multiple levels exists???)
 func _on_start_pressed() -> void:
-	AudioManager.play_sound("res://Sounds/UI/MenuSelect.ogg", 6, 2.5)
+	pass
 
 func _on_start_button_mouse_entered() -> void:
-	AudioManager.play_sound("res://Sounds/UI/MenuSelect.ogg", 6, 2)
+	pass
 
 func _on_start_button_mouse_exited() -> void:
-	AudioManager.play_sound("res://Sounds/UI/MenuSelect.ogg", 6, 2)
+	pass
