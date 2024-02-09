46c52d0191700c702a5b7263ebf6f26e9a6e1d62
William <william.alm.05@gmail.com>
2024-02-09 12:54:38 +0100

    This commit includes all the suggestions given by Xix.
    The collision layer two has now been given a name 'Player'. The variable damage_rate has been moved to the top. The hurtbox is now larger so it can detect the enemies, before they would just bounce off.

--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 signal health_depleted
 
+const DAMAGE_RATE = 5.0
+
 var health = 100.0
 
 func _physics_process(delta):
@@ -9,11 +11,9 @@ func _physics_process(delta):
 	velocity = direction * 600
 	move_and_slide()
 	
-	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %Hurtbox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
 		%Healthbar.value = health
 		if health <= 0.0:
 			health_depleted.emit()
-
