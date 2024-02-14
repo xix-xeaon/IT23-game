471b2955449f72ee2585f60e929920ea43439cb5
ra1nns <noell.jansson@elev.ga.ntig.se>
2024-02-14 10:32:55 +0100

    movement on mob
    changed the variable to an export variable

--- Mob/mob.gd
+++ Mob/mob.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-
+@export var speed = 0
 
 func _ready():
 	Mobtarget.player
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(Mobtarget.player.global_position)
-	velocity = direction * 300.0
+	velocity = direction * speed
 	move_and_slide()
