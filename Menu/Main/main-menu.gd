54689a183575048368dca40eacbb688e2353a2c3
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-02-16 11:18:18 +0100

    Change folder direction in main_menu script

--- Menu/Main/main-menu.gd
+++ Menu/Main/main-menu.gd
@@ -3,7 +3,7 @@ extends Control
 # Start Button
 func _on_start_pressed() -> void:
 	%MenuSelect.play()
-	get_tree().change_scene_to_file("res://Game/game.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")
 	
 func _on_start_button_mouse_exited() -> void:
 	%MenuSelect.play()
