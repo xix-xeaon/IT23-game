0b75f9b56da6f40cc334845144f9fbf610af3c3b
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-02-23 17:27:33 +0100

    Delete ghost-scene from this branch

--- Ghost/ghost.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-@onready var player 
-
-func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 350
-	move_and_slide()
