21d83952d32ca58a5b158e4f3ea7f7ccdc0b6873
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-02-29 11:51:34 +0100

    Update ghost.gd script
    Delete var player and add Global.player instead.

--- Ghost/ghost.gd
+++ Ghost/ghost.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-@onready var player 
+
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
+	var direction = global_position.direction_to(Global.player.global_position)
 	velocity = direction * 350
 	move_and_slide()
