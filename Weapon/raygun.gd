6bfe2140cba30fece21426c029845dc1c1c5241a
ra1nns <noell.jansson@elev.ga.ntig.se>
2024-02-29 14:21:27 +0100

    First Weapon
    Added a new weapon that has autotarget and can kill enemies

---
+++ Weapon/raygun.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+func _physics_process(delta):
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target_enemy = enemies_in_range.front()
+		look_at(target_enemy.global_position)
+		
+		
+
+func shoot():
+	const BEAM = preload("res://beam.tscn") 
+	var new_beam = BEAM.instantiate()
+	new_beam.global_position = %shootingpoint.global_position
+	new_beam.global_rotation = %shootingpoint.global_rotation
+	%shootingpoint.add_child(new_beam)
+
+
+func _on_timer_timeout():
+	shoot()
