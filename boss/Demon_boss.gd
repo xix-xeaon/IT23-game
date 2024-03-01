d6319371bc4a4aaa8b6cb61d46ff94b600315d04
Dannyq987 <156206548+Dannyq987@users.noreply.github.com>
2024-03-01 11:23:37 +0100

    A possible boss for the game.
    Currently working on animationsprite for the boss and possble ways for the boss to attack the player. But if you guys think this is a good sprite for the boss or have another option let me know.

---
+++ boss/Demon_boss.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(Global.player.global_position)
+	velocity = direction * speed 
+	move_and_slide()
+	
+	
+	
+		
