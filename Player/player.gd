4829143e43152def5e11e244cd391b14260b0a1a
ra1nns <noell.jansson@elev.ga.ntig.se>
2024-03-01 12:19:49 +0100

    Removed line of code
    Removed func _ready since it apparantly already exists

--- Player/player.gd
+++ Player/player.gd
@@ -9,8 +9,6 @@ var health = 100.0
 func _ready():
 	Global.player = self
 
-func _ready():
-	Global.player = self
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
