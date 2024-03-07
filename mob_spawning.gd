767e444c57aa69f5df326d311d169e65dabd5692
DRubnnn <ganvallruben@gmail.com>
2024-03-07 11:45:08 +0100

    Fixed mob spawning
    Fixed so mobs always spawn outside of the camera

--- mob_spawning.gd
+++ mob_spawning.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+
 func _on_timer_timeout():
 	spawn_mob()
 	
 	
 	
 func spawn_mob():
+	global_position = Global.player.global_position
 	var mob_spawn = preload("res://Mob/mob.tscn").instantiate()
 	$Path2D/PathFollow2D.progress_ratio = randf()
 	mob_spawn.global_position = $Path2D/PathFollow2D.global_position
