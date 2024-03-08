9c228ec624a2a1a433a0def301c41424978d7f38
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-03-08 10:53:36 +0100

    Update_animation for #pr10

--- Ghost/ghost.gd
+++ Ghost/ghost.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+func _ready():
+	%AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(Global.player.global_position)
