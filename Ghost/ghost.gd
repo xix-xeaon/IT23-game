96a88142d4dc29d66c3a4a2c4ca3383dc71eec2d
lawin666 <lawinamedi29@gmail.com>
2024-03-20 10:18:16 +0100

    Now enemies can take damage
    I created a health variabel and for mob and other enemies. Spider have 3hp, mob have 1hp and ghost have 1hp.

--- Ghost/ghost.gd
+++ Ghost/ghost.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
-
+@export var health = 3
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(Global.player.global_position)
 	velocity = direction * 350
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
--- Spider (mob)/spider.gd
+++ Spider (mob)/spider.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 200
+@export var health = 2
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(Global.player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
